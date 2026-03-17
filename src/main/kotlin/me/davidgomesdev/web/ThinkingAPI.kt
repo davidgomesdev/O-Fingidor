2c89b2215bec935a37ae5d7b28e3a9fb11b1d853
David Gomes <david.gomes@daimlertruck.com>
2026-03-17 19:50:53 +0000

    feat: add multipersonas

--- src/main/kotlin/me/davidgomesdev/web/ThinkingAPI.kt
+++ src/main/kotlin/me/davidgomesdev/web/ThinkingAPI.kt
@@ -1,5 +1,6 @@
 package me.davidgomesdev.web
 
+import io.netty.handler.codec.http.HttpResponseStatus
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.SpanKind
 import io.smallrye.common.annotation.Blocking
@@ -8,12 +9,14 @@ import jakarta.ws.rs.PUT
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import me.davidgomesdev.llm.PersonaContext
+import me.davidgomesdev.model.Persona
 import me.davidgomesdev.service.ChatService
 import org.jboss.logging.Logger
 import org.jboss.resteasy.reactive.RestMulti
 
 @Path("/pensa")
-class ThinkingAPI(val chatService: ChatService) {
+class ThinkingAPI(val chatService: ChatService, val personaContext: PersonaContext) {
 
     private val tracer = GlobalOpenTelemetry.getTracer(this::class.java.name)
     val log: Logger = Logger.getLogger(this::class.java)
@@ -22,9 +25,23 @@ class ThinkingAPI(val chatService: ChatService) {
     @Blocking
     @Produces(MediaType.TEXT_PLAIN)
     fun queryModel(body: QueryPayload): Multi<String> {
-        val span = tracer.spanBuilder("API QueryModel")
+        if (body.persona.isBlank()) {
+            return createResponse(HttpResponseStatus.BAD_REQUEST, "persona must be present")
+        }
+        personaContext.persona = Persona.entries.firstOrNull { it.codeName == body.persona } ?: return createResponse(
+            HttpResponseStatus.NOT_FOUND,
+            "persona not found"
+        )
+
+        val span = tracer.spanBuilder("API QueryModel").apply {
+            personaContext.persona!!.also { persona ->
+                log.info("Using persona: $persona")
+                setAttribute("persona", persona.codeName)
+            }
+        }
             .setSpanKind(SpanKind.INTERNAL)
             .startSpan()
+
         val traceId = span.spanContext.traceId
 
         log.info("Querying model with trace ID: $traceId")
@@ -35,4 +52,8 @@ class ThinkingAPI(val chatService: ChatService) {
     }
 }
 
-data class QueryPayload(val input: String)
+private fun createResponse(statusCode: HttpResponseStatus, text: String): RestMulti<String> =
+    RestMulti.fromMultiData(Multi.createFrom().item(text))
+        .status(statusCode.code()).build()
+
+data class QueryPayload(val input: String, val persona: String)
