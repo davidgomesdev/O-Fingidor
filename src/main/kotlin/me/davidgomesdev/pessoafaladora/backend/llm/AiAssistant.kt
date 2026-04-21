1e4402cac5a307ff8e9325efefc4f983d2261d55
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2026-04-21 20:04:47 +0100

    feat: add system messages for the personas

--- src/main/kotlin/me/davidgomesdev/pessoafaladora/backend/llm/AiAssistant.kt
+++ src/main/kotlin/me/davidgomesdev/pessoafaladora/backend/llm/AiAssistant.kt
@@ -16,55 +16,52 @@ import me.davidgomesdev.pessoafaladora.backend.service.Assistant
 import org.jboss.logging.Logger
 import java.io.File
 
-private const val PROMPT_FILE_NAME = "system_message.txt"
-
 @ApplicationScoped
 class AiAssistant(val personaContext: PersonaContext) {
 
     val log: Logger = Logger.getLogger(this::class.java)
-
-    // Used if there is no local system_message.txt
-    private val systemMessage: String =
-        Thread.currentThread().contextClassLoader
-            .getResourceAsStream("prompts/$PROMPT_FILE_NAME")!!
-            .reader().readText()
+    private val systemMessages: Map<String, String> = getPersonaSystemMessages()
 
     @Singleton
     @Suppress("unused")
     fun assistant(chatModel: StreamingChatModel, retrievalAugmentor: RetrievalAugmentor): Assistant {
         log.info("Creating assistant")
-        return AiServices.builder(Assistant::class.java)
-            .systemMessageProvider { _ ->
-                if (personaContext.persona == Persona.NINGUEM) return@systemMessageProvider null
-
-                val systemMessageLocalFile = File(PROMPT_FILE_NAME)
-
-                if (systemMessageLocalFile.exists()) return@systemMessageProvider systemMessageLocalFile.readText()
-
-                systemMessage
-            }
-            .registerListeners(
-                AiServiceStartedListener { event ->
-                    span().addEvent(
-                        "LLM query",
-                        attributes {
-                            put(
-                                "user_message",
-                                event.userMessage().contents()
-                                    .filterIsInstance<TextContent>()
-                                    .joinToString("\n\n", transform = TextContent::text)
-                            )
-                            put(
-                                "system_message", event.systemMessage()
-                                    .map(SystemMessage::text)
-                                    .orElseGet { "" })
-                        })
-                },
-                AiServiceErrorListener { error ->
-                    span().recordException(error.error())
+        return AiServices.builder(Assistant::class.java).systemMessageProvider { _ ->
+            resolveSystemMessage()
+        }.registerListeners(AiServiceStartedListener { event ->
+            span().addEvent(
+                "LLM query", attributes {
+                    put(
+                        "user_message",
+                        event.userMessage().contents().filterIsInstance<TextContent>()
+                            .joinToString("\n\n", transform = TextContent::text)
+                    )
+                    put(
+                        "system_message", event.systemMessage().map(SystemMessage::text).orElseGet { "" })
                 })
-            .streamingChatModel(chatModel)
-            .retrievalAugmentor(retrievalAugmentor)
-            .build()
+        }, AiServiceErrorListener { error ->
+            span().recordException(error.error())
+        }).streamingChatModel(chatModel).retrievalAugmentor(retrievalAugmentor).build()
+    }
+
+    private fun getPersonaSystemMessages(): Map<String, String> = buildMap {
+        val allFiles = Persona.entries.map(Persona::systemPromptFilename)
+        allFiles.forEach { fileName ->
+            val stream = Thread.currentThread().contextClassLoader.getResourceAsStream("prompts/$fileName")
+                ?: throw IllegalStateException("Prompt '$fileName' not found")
+
+            stream.reader().readText().let { put(fileName, it) }
+        }
+    }
+
+    private fun resolveSystemMessage(): String {
+        val persona = personaContext.persona ?: throw IllegalStateException("Persona not set")
+        val fileName = persona.systemPromptFilename
+
+        // Allow local file override for dev (e.g. system_message_alberto_caeiro.txt in working dir)
+        val localFile = File(fileName)
+        if (localFile.exists()) return localFile.readText()
+
+        return systemMessages[fileName] ?: throw IllegalStateException("Prompt for Persona '$persona' not found")
     }
 }
--- src/main/kotlin/me/davidgomesdev/pessoafaladora/backend/model/Persona.kt
+++ src/main/kotlin/me/davidgomesdev/pessoafaladora/backend/model/Persona.kt
@@ -1,14 +1,20 @@
 package me.davidgomesdev.pessoafaladora.backend.model
 
 @Suppress("unused")
-enum class Persona(val displayName: String, val category: PersonaCategory) {
-    NINGUEM("Ninguém", PersonaCategory.DEV),
+enum class Persona(
+    val displayName: String,
+    val category: PersonaCategory,
+    val systemPromptFilename: String = "system_prompt.txt"
+) {
+    /**
+     * Skips RAG
+     */
     O_FINGIDOR("O Fingidor", PersonaCategory.DEV),
     FERNANDO_PESSOA("Fernando Pessoa", PersonaCategory.ORTONIMO),
-    ALBERTO_CAEIRO("Alberto Caeiro", PersonaCategory.HETERONIMO),
-    ALVARO_DE_CAMPOS("Álvaro de Campos", PersonaCategory.HETERONIMO),
-    RICARDO_REIS("Ricardo Reis", PersonaCategory.HETERONIMO),
-    BERNARDO_SOARES("Bernardo Soares", PersonaCategory.SEMI_HETERONIMO);
+    ALBERTO_CAEIRO("Alberto Caeiro", PersonaCategory.HETERONIMO, "system_message_alberto_caeiro.txt"),
+    ALVARO_DE_CAMPOS("Álvaro de Campos", PersonaCategory.HETERONIMO, "system_message_alvaro_de_campos.txt"),
+    RICARDO_REIS("Ricardo Reis", PersonaCategory.HETERONIMO, "system_message_ricardo_reis.txt"),
+    BERNARDO_SOARES("Bernardo Soares", PersonaCategory.SEMI_HETERONIMO, "system_message_bernardo_soares.txt");
 
     val codeName = this.name.lowercase()
 }
