70acf773a8236130a929ab04318b647b4027ebb9
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2026-04-24 21:32:38 +0100

    fix: wrong filename for system message

--- src/main/kotlin/me/davidgomesdev/pessoafaladora/backend/model/Persona.kt
+++ src/main/kotlin/me/davidgomesdev/pessoafaladora/backend/model/Persona.kt
@@ -3,20 +3,28 @@ package me.davidgomesdev.pessoafaladora.backend.model
 @Suppress("unused")
 enum class Persona(
     val displayName: String,
-    val category: PersonaCategory,
-    val systemPromptFilename: String = "system_prompt.txt"
+    val category: PersonaCategory
 ) {
+
     /**
      * Skips RAG
      */
     O_FINGIDOR("O Fingidor", PersonaCategory.DEV),
     FERNANDO_PESSOA("Fernando Pessoa", PersonaCategory.ORTONIMO),
-    ALBERTO_CAEIRO("Alberto Caeiro", PersonaCategory.HETERONIMO, "system_message_alberto_caeiro.txt"),
-    ALVARO_DE_CAMPOS("Álvaro de Campos", PersonaCategory.HETERONIMO, "system_message_alvaro_de_campos.txt"),
-    RICARDO_REIS("Ricardo Reis", PersonaCategory.HETERONIMO, "system_message_ricardo_reis.txt"),
-    BERNARDO_SOARES("Bernardo Soares", PersonaCategory.SEMI_HETERONIMO, "system_message_bernardo_soares.txt");
+    ALBERTO_CAEIRO("Alberto Caeiro", PersonaCategory.HETERONIMO),
+    ALVARO_DE_CAMPOS("Álvaro de Campos", PersonaCategory.HETERONIMO),
+    RICARDO_REIS("Ricardo Reis", PersonaCategory.HETERONIMO),
+    BERNARDO_SOARES("Bernardo Soares", PersonaCategory.SEMI_HETERONIMO);
 
     val codeName = this.name.lowercase()
+
+    val systemPromptFilename: String
+        get() {
+            if (this == O_FINGIDOR || this == FERNANDO_PESSOA) {
+                return "system_message.txt"
+            }
+            return "system_message_$codeName.txt"
+        }
 }
 
 @Suppress("unused")
