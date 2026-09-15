46f6354606203fbc7d5542bdd5844bce2f68212f
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2026-09-15 11:10:56 +0100

    feat: add date tool

---
+++ backend/src/main/kotlin/me/davidgomesdev/ofingidor/backend/llm/tool/DateTool.kt
@@ -0,0 +1,28 @@
+package me.davidgomesdev.ofingidor.backend.llm.tool
+
+import dev.langchain4j.agent.tool.Tool
+import java.time.LocalDate
+import java.time.Month
+
+class DateTool {
+    @Tool("Diz a data do dia de hoje")
+    fun todayDate(): String =
+        LocalDate.now().run {
+            "$dayOfMonth de ${month.toPortuguese()} de $year"
+        }
+}
+
+fun Month.toPortuguese(): String = when (this) {
+    Month.JANUARY -> "Janeiro"
+    Month.FEBRUARY -> "Fevereiro"
+    Month.MARCH -> "Março"
+    Month.APRIL -> "Abril"
+    Month.MAY -> "Maio"
+    Month.JUNE -> "Junho"
+    Month.JULY -> "Julho"
+    Month.AUGUST -> "Agosto"
+    Month.SEPTEMBER -> "Setembro"
+    Month.OCTOBER -> "Outubro"
+    Month.NOVEMBER -> "Novembro"
+    Month.DECEMBER -> "Dezembro"
+}
